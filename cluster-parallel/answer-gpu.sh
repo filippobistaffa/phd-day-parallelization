c0bc118b99e01674fa48aee0285286d8ef4f4af1
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-11-10 14:15:49 +0100

    Updated scripts

---
+++ cluster-parallel/answer-gpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=llama-cpp-answer-gpu
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=10:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=12GB
+#SBATCH --output=answer-gpu.out
+#SBATCH --error=answer-gpu.err
+
+spack load cuda@11.8.0
+
+LLAMA_CPP_HOME="/home/filippo.bistaffa/phd-day-parallelization/cluster-parallel/llama.cpp"
+MODEL="${LLAMA_CPP_HOME}/models/vicuna-13b-v1.5-16k.Q4_K_M.gguf"
+
+PROMPT="The Answer to the Ultimate Question of Life, the Universe, and Everything is"
+
+srun ${LLAMA_CPP_HOME}/build/bin/main --model ${MODEL} --prompt "${PROMPT}" --escape --log-file answer-gpu --repeat_penalty 1.1 --ctx-size 4096 --n-predict -1 --temp 0.7 --seed 0 --n-gpu-layers 30
