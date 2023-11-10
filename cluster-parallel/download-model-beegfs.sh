96cdd420cabcfd2e9ff34175a7136568d046c482
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-11-10 16:48:09 +0100

    Updated code

--- cluster-parallel/download-model-beegfs.sh
+++ cluster-parallel/download-model-beegfs.sh
@@ -2,7 +2,9 @@
 
 BEEGFS_DIR="$HOME/beegfs/llm-models"
 LOCAL_DIR="llama.cpp/models"
+HF_REPO="TheBloke/vicuna-13B-v1.5-16K-GGUF"
+MODEL="vicuna-13b-v1.5-16k.Q4_K_M.gguf"
 
 mkdir -p $BEEGFS_DIR
-huggingface-cli download TheBloke/vicuna-13B-v1.5-16K-GGUF vicuna-13b-v1.5-16k.Q4_K_M.gguf --local-dir $BEEGFS_DIR --local-dir-use-symlinks False
+huggingface-cli download $HF_REPO $MODEL --local-dir $BEEGFS_DIR --local-dir-use-symlinks False
 ln -s $BEEGFS_DIR/vicuna-13b-v1.5-16k.Q4_K_M.gguf $LOCAL_DIR/vicuna-13b-v1.5-16k.Q4_K_M.gguf
