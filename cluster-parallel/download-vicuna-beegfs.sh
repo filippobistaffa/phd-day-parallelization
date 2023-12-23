9486fb0b3111bd6296c49c40003171470a1b5c91
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-12-23 15:21:08 +0100

    Better code

--- cluster-parallel/download-vicuna-beegfs.sh
+++ cluster-parallel/download-vicuna-beegfs.sh
@@ -7,4 +7,4 @@ MODEL="vicuna-13b-v1.5-16k.Q4_K_M.gguf"
 
 mkdir -p $BEEGFS_DIR
 huggingface-cli download $HF_REPO $MODEL --local-dir $BEEGFS_DIR --local-dir-use-symlinks False
-ln -s $BEEGFS_DIR/vicuna-13b-v1.5-16k.Q4_K_M.gguf $LOCAL_DIR/vicuna-13b-v1.5-16k.Q4_K_M.gguf
+ln -s $BEEGFS_DIR/$MODEL $LOCAL_DIR/$MODEL
