f21ef548140cd5b0690d30775453e6bc28622829
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-12-23 16:31:37 +0100

    Script to use `mixtral` with GPU

---
+++ cluster-parallel/description-mixtral-gpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=llama-cpp-description-mixtral-gpu
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=10:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=16G
+#SBATCH --output=description-mixtral-gpu-%j.out
+#SBATCH --error=description-mixtral-gpu-%j.err
+
+spack load cuda@11.8.0
+spack load --first py-pandas
+
+python3 description-mixtral.py --n-gpu-layers 32
